f4b39b131a6a2351ff760298c028aec70e82f500
Roy <dsf@sdfsd.com>
2019-11-10 23:28:38 +0800

    2019-11-10 23:28

--- angularSrc/src/app/show-roster/show-roster.module.ts
+++ angularSrc/src/app/show-roster/show-roster.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ShowRosterRoutingModule} from './show-roster-routing.module';
 import { ShowRosterComponent } from './show-roster.component';
 import { RosterTableComponent } from '../components/roster-table/roster-table.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [ShowRosterComponent,
                  RosterTableComponent],
   imports: [
     CommonModule,
-    ShowRosterRoutingModule
+    ShowRosterRoutingModule,
+    HttpClientModule 
   ]
 })
 export class ShowRosterModule { }
