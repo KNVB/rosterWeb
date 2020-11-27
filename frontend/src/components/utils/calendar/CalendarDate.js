07b3b07f99d11020da91a34ae13a214211cc2cd0
SITO3 <bb@cc.com>
2020-11-27 08:52:53 +0800

    Add Calendar utility packages

---
+++ frontend/src/components/utils/calendar/CalendarDate.js
@@ -0,0 +1,10 @@
+class CalendarDate
+{
+	constructor(){
+        this.publicHoliday=false;
+        this.today=false;
+        this.dateOfMonth=-1;
+        this.dayOfWeek="";
+	}	
+}
+export default CalendarDate;
