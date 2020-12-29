9322703daca7634ff695066d4585dec985635c59
SITO3 <bb@cc.com>
2020-12-29 11:02:34 +0800

    Empty the workspace

--- angularSrc/dist/out-tsc/src/app/classes/monthly-calendar.spec.js
+++
@@ -1,7 +0,0 @@
-import { MonthlyCalendar } from './monthly-calendar';
-describe('MonthlyCalendar', () => {
-    it('should create an instance', () => {
-        expect(new MonthlyCalendar()).toBeTruthy();
-    });
-});
-//# sourceMappingURL=monthly-calendar.spec.js.map
