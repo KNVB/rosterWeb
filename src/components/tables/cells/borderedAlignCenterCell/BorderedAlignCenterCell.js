beabd93857d2e90dfff7656ba660ed9afe3e8f45
SITO3 <bb@cc.com>
2021-02-23 09:18:41 +0800

    Fix the CSS class inheritance problem

--- src/components/tables/cells/borderedAlignCenterCell/BorderedAlignCenterCell.js
+++ src/components/tables/cells/borderedAlignCenterCell/BorderedAlignCenterCell.js
@@ -1,8 +1,9 @@
 import BorderedCell from '../borderedCell/BorderedCell';
 export default function BorderedAlignCenterCell(props){
-    return(
-      <BorderedCell {...props} className={props.className+" text-center"}>
-        {props.children}
-      </BorderedCell>
-    )
+  let cssClass="text-center"+((props.className)?" "+props.className:"");
+  return(
+    <BorderedCell {...props} className={cssClass}>
+      {props.children}
+    </BorderedCell>
+  )
 }
--- src/components/tables/cells/shiftCell/ShiftCell.js
+++ src/components/tables/cells/shiftCell/ShiftCell.js
@@ -3,7 +3,7 @@ import {useContext} from 'react';
 import BorderedAlignCenterCell from '../../cells/borderedAlignCenterCell/BorderedAlignCenterCell';
 import RosterWebContext from '../../../../RosterWebContext';
 export default function ShiftCell(props){
-    let shiftClass = props.className+" p-0 shiftCell";
+    let shiftClass ="p-0 shiftCell"+((props.className)?" "+props.className:"");
     let { activeShiftInfoList } = useContext(RosterWebContext);
     if (activeShiftInfoList[props.children]) {
         shiftClass +=  " " + activeShiftInfoList[props.children].cssClassName;    
