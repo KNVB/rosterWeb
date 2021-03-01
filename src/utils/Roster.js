71c7349505374e8e351cef12b187bf0b3852504c
Roy <roy.cstsang@gmail.com>
2021-03-01 21:51:03 +0800

    add Roster.js

---
+++ src/utils/Roster.js
@@ -0,0 +1,14 @@
+import Utility from './Utility';
+export default class Roster{
+    constructor(){
+        this.get=(year,month)=>{
+           return Utility.fetchAPI('/publicAPI/getITORosterList','GET',{"year":year,"month":month});
+        }
+        this.getAllActiveShiftInfo=()=>{
+            return Utility.fetchAPI('/publicAPI/getAllActiveShiftInfo','GET');
+        }
+        this.getRosterSchedulerList=(year,month)=>{
+            return Utility.fetchAPI('/privateAPI/getRosterSchedulerList','POST',{"year":year,"month":month});
+        }
+    }
+}
