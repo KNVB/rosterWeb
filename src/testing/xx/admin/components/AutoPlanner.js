7ff70246210f4acf5b7176155a8223a0f3b02951
Roy <roy.cstsang@gmail.com>
2021-05-24 22:42:47 +0800

    Setup the layout of the auto planner.

--- src/testing/xx/admin/components/AutoPlanner.js
+++ src/testing/xx/admin/components/AutoPlanner.js
@@ -1,61 +1,44 @@
 import './AutoPlanner.css';
-import {Fragment} from "react";
+import {useContext} from 'react';
+import RosterWebContext from '../../utils/RosterWebContext';
 export default function AutoPlanner(props){
+    let [contextValue,updateContext]=useContext(RosterWebContext);
     return (
-        <Fragment>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            Auto Planning Start From:
-                        </td>
-                        <td colSpan="2">
-                            <select id="autoPlanStartDate">
-                            </select>
-                            &nbsp;to&nbsp;
-                            <select id="autoPlanEndDate"></select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Iteration Count:</td>
-                        <td><input id="iterationCount" type="number"/></td>
-                        <td>&nbsp;<div className="autoPlannerButton">Auto Planner</div></td>
-                    </tr>
-                </tbody>
-            </table>
-            <div id="genResult" style={{"padding-left": "10px"}}>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td colSpan="2">Standard Deviation:</td>
-                        </tr>
-                        <tr id="theLowestSD">
-                            <td>1</td><td>1</td>
-                        </tr>
-                        <tr id="secondLowestSD">
-                            <td>1</td><td>1</td>
-                        </tr>
-                        <tr id="thirdLowestSD">
-                            <td>1</td><td></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2"><br/></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">Missing shift Count:</td>
-                        </tr>
-                        <tr id="theLowestMissingShiftCount">
-                            <td>1</td><td>1</td>
-                        </tr>
-                        <tr id="theSecondLowestMissingShiftCount">
-                            <td>1</td><td>1</td>
-                        </tr>
-                        <tr id="theThirdLowestMissingShiftCount">
-                            <td>1</td><td>1</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </Fragment>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        Auto Planning Start From:
+                    </td>
+                    <td colSpan="3">
+                        <select id="autoPlanStartDate">
+                        </select>
+                        &nbsp;to&nbsp;
+                        <select id="autoPlanEndDate"></select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Iteration Count:</td>
+                    <td><input id="iterationCount" type="number"/></td>
+                    <td colSpan="2">&nbsp;<div className="autoPlannerButton">Auto Planner</div></td>
+                </tr>
+                <tr>
+                    <td colSpan="2">Standard Deviation:</td>
+                    <td colSpan="2">Missing shift Count:</td>
+                </tr>
+                <tr id="theLowestSD">
+                    <td>1</td><td>1</td>
+                    <td>1</td><td>1</td>
+                </tr>
+                <tr id="thirdLowestSD">
+                    <td>1</td><td></td>
+                    <td colSpan="2"><br/></td>
+                </tr>
+                <tr id="theLowestMissingShiftCount">
+                    <td>1</td><td>1</td>
+                    <td>1</td><td>1</td>
+                </tr>
+            </tbody>
+        </table>
     );
 }
