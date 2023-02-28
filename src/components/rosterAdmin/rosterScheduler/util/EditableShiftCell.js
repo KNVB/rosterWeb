a2d195d7cf7caed14295d3159299234336f4a655
Roy Tsang <cstsang@hko.gov.hk>
2023-02-28 10:29:53 +0800

    Before Selected Region feature Implementation

--- src/components/rosterAdmin/rosterScheduler/util/EditableShiftCell.js
+++ src/components/rosterAdmin/rosterScheduler/util/EditableShiftCell.js
@@ -1,6 +1,7 @@
 import ShiftCell from "../../../cells/ShiftCell";
 export default function EditableShiftCell(props) {
     let {children,cssClassName,onBlur, setIsHighLightRow,updateHighLightCellIndex,updateSelectedRegion}=props;
+    cssClassName+=" editableShiftCell";
     return (
         <ShiftCell
             cssClassName={cssClassName}
