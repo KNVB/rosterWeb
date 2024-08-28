2dd37eed78358a60f4095a7e0276d960d7540077
Roy Tsang <cstsang@hko.gov.hk>
2024-08-28 11:31:02 +0800

    Extract Calendar Table object

---
+++ src/components/common/dateTimePicker/CalendarTable.js
@@ -0,0 +1,56 @@
+export default function CalendarTable({
+    monthFullNameList,
+    monthlyCalendar,
+    nextMonth,
+    nextYear,
+    prevMonth,
+    prevYear,
+    selectedDate,
+    setSelectedDate,
+    weekDayNameList }) {
+    let calendarRows = [];
+    if (monthlyCalendar !== null) {
+        monthlyCalendar.forEach((row, rowIndex) => {
+            let calendarRow = [];
+            row.forEach((d, columIndex) => {
+                let className = [];
+
+                if (d === selectedDate.getDate()) {
+                    className.push("selectedDate");
+                } else {
+                    if (d !== "") {
+                        className.push("clickable");
+                    }
+                }
+                calendarRow.push(<td key={rowIndex + "_" + columIndex} className={className.join(" ")} onClick={() => setSelectedDate(d)}>{d}</td>)
+            });
+            calendarRows.push(<tr key={"dateTime_" + rowIndex}>{calendarRow}</tr>);
+        });
+    }
+    return (
+        <table className="calendarTable">
+            <thead>
+                <tr>
+                    <td className="clickable text-start" onClick={prevYear}>&lt;&lt;</td>
+                    <td className="clickable text-start" onClick={prevMonth}>&lt;</td>
+                    <td colSpan={3}>
+                        {monthFullNameList[selectedDate.getMonth()]}&nbsp;
+                        {selectedDate.getFullYear()}
+                    </td>
+                    <td className="clickable text-end" onClick={nextMonth}>&gt;</td>
+                    <td className="clickable text-end" onClick={nextYear}>&gt;&gt;</td>
+                </tr>
+                <tr className="dayRow">
+                    {
+                        weekDayNameList.map(weekDayName => (
+                            <td key={weekDayName}>{weekDayName}</td>
+                        ))
+                    }
+                </tr>
+            </thead>
+            <tbody>
+                {calendarRows}
+            </tbody>
+        </table>
+    )
+}
--- src/components/common/dateTimePicker/DateTimePicker.js
+++ src/components/common/dateTimePicker/DateTimePicker.js
@@ -1,4 +1,5 @@
 import "./DateTimePicker.css";
+import CalendarTable from "./CalendarTable";
 import useDateTimePicker from "./useDateTimePicker";
 export default function DateTimePicker({ value }) {
     let dateFormatter = new Intl.DateTimeFormat('en-ZA', {
@@ -10,54 +11,22 @@ export default function DateTimePicker({ value }) {
         year: "numeric"
     });
     const { isShowPicker, monthFullNameList, monthlyCalendar, result, weekDayNameList, action } = useDateTimePicker(value);
-    let calendarRows = [];
-    if (monthlyCalendar !== null) {
-        monthlyCalendar.forEach((row,rowIndex) => {
-            let calendarRow = [];
-            row.forEach((d,columIndex) => {
-                let className=[];
-
-                if (d === result.getDate()) {
-                    className.push("selectedDate");
-                } else {
-                    if (d !== ""){
-                        className.push("clickable"); 
-                    }
-                }
-                calendarRow.push(<td key={rowIndex+"_"+columIndex} className={className.join(" ")} onClick={()=>action.setSelectedDate(d)}>{d}</td>)
-            });
-            calendarRows.push(<tr key={"dateTime_"+rowIndex}>{calendarRow}</tr>);
-        });
-    }
+   
     return (
         <div className="dateTimePicker">
             <div className="dateTimeResult" onClick={action.showPicker}>{dateFormatter.format(result)}</div>
             {isShowPicker &&
                 <div className="pickerContainer">
-                    <table className="calendarTable">
-                        <thead>
-                            <tr>
-                                <td className="clickable text-start" onClick={action.prevYear}>&lt;&lt;</td>
-                                <td className="clickable text-start" onClick={action.prevMonth}>&lt;</td>
-                                <td colSpan={3}>
-                                    {monthFullNameList[result.getMonth()]}&nbsp;
-                                    {result.getFullYear()}
-                                </td>
-                                <td className="clickable text-end" onClick={action.nextMonth}>&gt;</td>
-                                <td className="clickable text-end" onClick={action.nextYear}>&gt;&gt;</td>
-                            </tr>
-                            <tr className="dayRow">
-                                {
-                                    weekDayNameList.map(weekDayName => (
-                                        <td key={weekDayName}>{weekDayName}</td>
-                                    ))
-                                }
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {calendarRows}
-                        </tbody>
-                    </table>
+                   <CalendarTable 
+                        monthFullNameList={monthFullNameList}
+                        monthlyCalendar={monthlyCalendar}
+                        nextMonth={action.nextMonth}
+                        nextYear={action.nextYear}
+                        prevMonth={action.prevMonth}
+                        prevYear={action.prevYear}
+                        selectedDate={result}
+                        setSelectedDate={action.setSelectedDate}
+                        weekDayNameList={weekDayNameList}/>
                 </div>
             }
         </div>
