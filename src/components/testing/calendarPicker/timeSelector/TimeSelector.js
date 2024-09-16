20160d683ffad4bd586b409fe4dce4af935ce437
Roy Tsang <cstsang@hko.gov.hk>
2024-09-16 18:05:44 +0800

    今晚繼續

--- src/components/testing/calendarPicker/timeSelector/TimeSelector.js
+++ src/components/testing/calendarPicker/timeSelector/TimeSelector.js
@@ -1,43 +1,59 @@
 import "./TimeSelector.css";
-import useTimeSelector from "./useTimeSelector"
+import useTimeSelector from "./useTimeSelector";
 export default function TimeSelector({ getSelectedTime, value }) {
     const { aPM, hour, minute, selectedItem, action } = useTimeSelector(value);
-    let downValue = () => {
-        switch (selectedItem) {
-            case "APM":
-                toggleAPM();
-                break;
-            case "hour":
-                getSelectedTime(action.downHour());
-                break;
-            case "minute":
-                getSelectedTime(action.downMin());
-                break;
-            default:
-                break;
-        }
+    let handleClick = (fieldName, element) => {
+        action.updateSelectedItem(fieldName);
+        let value = element.value;
+        element.value = "";
+        element.value = value;
     }
+    let updateHourFieldValue = (element) => {
+        let temp = element.value;
+        let result;
+        if (Number(temp) < Number(element.max)) {
+            if ((temp === "0") || (temp === "")) {
+                result = 12;
+            } else {
+                result = Number(temp);
+            }
+        } else {
+            switch (true){
+                case (Number(temp) === Number(element.max)):
+                    result=1;
+                    break;
+                case (temp === "0") :
+                        
 
-    let toggleAPM = () => {
-        getSelectedTime(action.toggleAPM());
-    }
-    let updateTimeFieldValue=(fieldName, value) => {
-        getSelectedTime(action.updateTimeFieldValue(fieldName, value));
-    }
-    let upValue = () => {
-        switch (selectedItem) {
-            case "APM":
-                toggleAPM();
-                break;
-            case "hour":
-                getSelectedTime(action.upHour());
-                break;
-            case "minute":
-                getSelectedTime(action.upMin());
-                break;
-            default:
-                break;
+            }
+            /*
+            swith (true) 
+                
+
+            }
+            
+            if 
+                temp=1;
+            temp = temp.substring(temp.length - 1);
+            
+            if (temp === "0") {
+                result = 12;
+            } else {
+                result = Number(temp);
+            }*/
         }
+        getSelectedTime(action.updateTimeFieldValue("hour" , String(result).padStart(2, '0')));
+    }
+    let updateMinuteField=element=>{
+        let temp = element.value;
+        let result;
+        if (Number(temp) < Number(element.max)) {
+            result = Number(temp);
+        }else {
+            temp = temp.substring(temp.length - 1);
+            result = Number(temp);
+        }
+        getSelectedTime(action.updateTimeFieldValue("minute" , String(result).padStart(2, '0')));
     }
     return (
         <div className="timeSelector">
@@ -45,44 +61,18 @@ export default function TimeSelector({ getSelectedTime, value }) {
                 className={"timeSelectorDigit" + (selectedItem === "hour" ? " selectedItem" : "")}
                 max="13"
                 min="0"
-                onChange={e => updateTimeFieldValue("hour", e.target.value)}
-                onClick={() => action.updateSelectedItem("hour")}
+                onChange={e => updateHourFieldValue(e.target)}
+                onClick={e => handleClick("hour", e.target)}
                 type="number"
                 value={hour} />&nbsp;:&nbsp;
             <input
                 className={"timeSelectorDigit" + (selectedItem === "minute" ? " selectedItem" : "")}
                 max="60"
                 min="-1"
-                onChange={e => updateTimeFieldValue("minute", e.target.value)}
-                onClick={() => action.updateSelectedItem("minute")}
+                onChange={e => updateMinuteField(e.target)}
+                onClick={e => handleClick("minute", e.target)}
                 type="number"
                 value={minute} />&nbsp;
-            <span
-                className={"timeSelectorDigit" + (selectedItem === "APM" ? " selectedItem" : "")}
-                onClick={() => action.updateSelectedItem("APM")}
-                suppressContentEditableWarning={true}>{aPM}</span>
-            {/*           <span
-                className={"timeSelectorDigit" + (selectedItem === "hour" ? " selectedItem" : "")}
-                contentEditable={true}
-                onClick={() => action.updateSelectedItem("hour")}
-                onKeyUp={e =>{ return handleKeyBoardEvent(e, "hour");}}
-                suppressContentEditableWarning={true}
-            >{hour}</span>&nbsp;:&nbsp;
-            <span
-                className={"timeSelectorDigit" + (selectedItem === "minute" ? " selectedItem" : "")}
-                contentEditable={true}
-                onClick={() => action.updateSelectedItem("minute")}
-                suppressContentEditableWarning={true}>{minute}</span>&nbsp;
-            <span
-                className={"timeSelectorDigit" + (selectedItem === "APM" ? " selectedItem" : "")}
-                contentEditable={true}
-                onClick={() => action.updateSelectedItem("APM")}
-                suppressContentEditableWarning={true}>{aPM}</span>
-*/}
-            <div className="timeSelectorUpDown">
-                <div onClick={upValue}>▲</div>
-                <div onClick={downValue}>▼</div>
-            </div>
         </div>
-    )
+    );
 }
